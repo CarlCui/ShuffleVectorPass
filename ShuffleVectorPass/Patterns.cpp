8078b5767bdbc0f10df0b617f6e95bb36c3129bf
Carl Cui <carlcui1993@outlook.com>
2018-02-26 14:44:54 -0800

    basic sequential matching for rotation

--- ShuffleVectorPass/Patterns.cpp
+++ ShuffleVectorPass/Patterns.cpp
@@ -5,7 +5,30 @@ using namespace llvm;
 
 bool RotationPattern::matches(ShuffleVectorInst *inst) {
     errs() << "rotation pattern\n";
-    return false;
+
+    auto mask = inst->getShuffleMask();
+
+    auto maskSize = mask.size();
+
+    for (unsigned i = 0; i < maskSize; i ++) {
+        mask[i] = i - mask[i];
+
+        if (mask[i] < 0) {
+            mask[i] = mask[i] + maskSize;
+        }
+    }
+
+    bool allEqual = true;
+
+    for (unsigned i = 0; i < maskSize; i ++) {
+        if (mask[i] != mask[0]) {
+            allEqual = false;
+        }
+    }
+
+    errs() << allEqual << "\n";
+
+    return allEqual;
 }
 
 bool RotationPattern::optimize(ShuffleVectorInst *inst) {
