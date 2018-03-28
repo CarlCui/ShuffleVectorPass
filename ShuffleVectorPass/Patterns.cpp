8bfe0b062a069368d5c92ff9c67bac3057aa8761
Carl Cui <carlcui1993@outlook.com>
2018-03-27 21:03:35 -0700

    optimized algorithm

--- ShuffleVectorPass/Patterns.cpp
+++ ShuffleVectorPass/Patterns.cpp
@@ -32,58 +32,35 @@ bool RotationPattern::matches(ShuffleVectorInst *inst) {
 
     auto maskSize = mask.size();
 
-    const int fw = 8;
+    const int fw = 128 / 16;
 
     uint8_t maskData[16] = {}; // 16 x i8, zero initialized
     uint8_t indeces[16] = {}; // an array of indeces ([0, 1, 2, ...])
-    uint8_t lArr[16] = {};
     uint8_t lengthMaskArr[16] = {};
 
     for (unsigned i = 0; i < maskSize; i ++) {
         maskData[i] = (uint8_t)mask[i];
         indeces[i] = i;
-        lArr[i] = maskSize;
         lengthMaskArr[i] = 0xFF;
     }
 
     BitBlock maskVector;
     BitBlock indexVector;
-    BitBlock constantLengthVector; // array of lengths
     BitBlock lengthMaskVector; //
 
     BitBlock zeroVector = mvmd128<fw>::fill(0);
 
     maskVector = bitblock128::load_aligned((BitBlock*)&maskData);
     indexVector = bitblock128::load_aligned((__m128i*)&indeces);
-    constantLengthVector = bitblock128::load_aligned((__m128i*)&lArr);
     lengthMaskVector = bitblock128::load_aligned((__m128i*)&lengthMaskArr);
 
-    print128_i8(maskVector);
-    print128_i8(indexVector);
-    print128_i8(constantLengthVector);
-    print128_i8(lengthMaskVector);
-
-    BitBlock diff = simd128<fw>::sub(indexVector, maskVector);
-    BitBlock modded = simd128<fw>::add(diff, constantLengthVector);
-    BitBlock cmpMask = simd128<fw>::gt(diff, zeroVector);
-    BitBlock result = simd128<fw>::ifh(cmpMask, diff, modded);
-
-    print128_i8(diff);
-    print128_i8(modded);
-    print128_i8(cmpMask);
-    print128_i8(result);
+    BitBlock result = simd_xor(maskVector, indexVector);
 
     int firstElement = mvmd128<fw>::extract<0>(result);
-
-    errs() << firstElement << "\n";
 
     BitBlock constantFirst = mvmd128<fw>::fill(firstElement);
     BitBlock constantFirstTillLength = simd128<fw>::ifh(lengthMaskVector, constantFirst, zeroVector);
     BitBlock allEqual = simd128<fw>::sub(result, constantFirstTillLength);
-
-    print128_i8(constantFirst);
-    print128_i8(constantFirstTillLength);
-    print128_i8(allEqual);
 
     bool allEqualResult  = bitblock::any(allEqual);
 
